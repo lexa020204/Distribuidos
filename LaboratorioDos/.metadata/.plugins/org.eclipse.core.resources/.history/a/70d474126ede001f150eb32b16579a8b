c99e69d5577290b40321f0c846dc4f933d3b7ad5
Alexa020204 <heartPink303@gmail.com>
2025-01-29 13:47:47 -0500

    cambios ♒

---
+++ LaboratorioDos/.metadata/.plugins/org.eclipse.core.resources/.history/a/70d474126ede001f150eb32b16579a8b
@@ -0,0 +1,17 @@
+package co.edu.up.classesclient;
+
+public class Client {
+	
+	JSocketClient sk;
+	
+	public Client(String address, int port) {
+		sk = new JSocketClient(address, port);		
+	}
+	
+	public void opetation(String data) {
+		
+		//aqui while el menu
+		this.sk.request(data);
+	}
+
+}
--- LaboratorioDos/Cliente/src/co/edu/up/classesclient/Client.java
+++ LaboratorioDos/Cliente/src/co/edu/up/classesclient/Client.java
@@ -1,17 +1,40 @@
 package co.edu.up.classesclient;
 
-public class Client {
-	
-	JSocketClient sk;
-	
-	public Client(String address, int port) {
-		sk = new JSocketClient(address, port);		
-	}
-	
-	public void opetation(String data) {
-		
-		//aqui while el menu
-		this.sk.request(data);
-	}
+import java.util.Scanner;
 
+public class Client {
+    
+    String opt;
+    String message;
+    JSocketClient sk;
+    public Scanner sc;
+
+    public Client(String address, int port) {
+        sk = new JSocketClient(address, port);    
+        sc = new Scanner(System.in);
+    }
+
+    public void message() {
+        while (true) {
+            // Muestra el menú siempre antes de pedir la opción
+            System.out.println("\n[Cliente]: Escoge una opción");
+            System.out.println("1. Enviar mensaje");
+            System.out.println("2. Cerrar conexión");
+
+            opt = sc.nextLine(); // Leer opción
+
+            if (opt.equals("1")) {
+                System.out.println("Ingrese un mensaje para enviar:");
+                message = sc.nextLine();
+                sk.request(message); // Enviar mensaje
+            } else if (opt.equals("2")) {
+                System.out.println("\n[Cliente]: Cerrando conexión...");
+                sk.closeService(); 
+                break; // Salir del bucle
+            } else {
+                System.out.println("[Cliente]: Opción no válida. Intente de nuevo.");
+            }
+        }
+        sc.close(); // Cerrar Scanner una vez fuera del bucle
+    }
 }
